99e09b48598b5441002b3d4fa8eed6415a63461c
kezagiselle <kezagiselle776@gmail.com>
2024-06-27 11:41:49 +0200

    am done

--- index.js
+++ index.js
@@ -22,3 +22,10 @@ while (rows.length < count) {
 for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));
 }
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
